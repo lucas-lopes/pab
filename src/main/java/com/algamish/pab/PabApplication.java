027ac613e8913883489e550259532e4a15ae941a
Lucas Barbosa <lucas.lopesifrn@gmail.com>
2021-02-15 16:04:00 -0300

    Initial commit

---
+++ src/main/java/com/algamish/pab/PabApplication.java
@@ -0,0 +1,13 @@
+package com.algamish.pab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabApplication.class, args);
+	}
+
+}
